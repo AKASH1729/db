9f70d131521aa88a5ceee0db79e6e542520fca61
AKASH SHUKLA <akashgarg200202@gmail.com>
2025-07-20 18:46:28 +0530

    correct the secret and db

--- secret.tf
+++ secret.tf
@@ -5,12 +5,12 @@ resource "random_password" "db_password" {
   override_special = "-!$&"
 }
 
-# Create a Secrets Manager secret to store the DB password
+# Create a new Secrets Manager secret with a fresh name
 resource "aws_secretsmanager_secret" "db_secret" {
-  name = "prod-db-password-v2"
+  name = "prod-db-password-v3"  # <-- Changed from v2 to v3
 }
 
-# Store the random password in Secrets Manager
+# Store the random password in the newly created secret
 resource "aws_secretsmanager_secret_version" "secret_version" {
   secret_id     = aws_secretsmanager_secret.db_secret.id
   secret_string = random_password.db_password.result
